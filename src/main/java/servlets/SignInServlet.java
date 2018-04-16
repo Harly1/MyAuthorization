0b2021a82fef056f5124e3b16ae3ff0cc569c738
harly <harly1@yandex.ru>
2018-04-16 09:59:28 +0300

    Work version

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -1,15 +1,37 @@
 package servlets;
 
+import accounts.AccountService;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SignInServlet extends HttpServlet {
+
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+
+        this.accountService = accountService;
+    }
+
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String login = request.getUserPrincipal().getName();
-        response.setContentType("text/html;charset=utf-8");
-        response.getWriter().println("Authorized: " + login);
-        response.setStatus(HttpServletResponse.SC_OK);
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        try {
+            String loginFromBD = accountService.getUserByLogin(login).getLogin();
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(200);
+            response.getWriter().println("Authorized: " + login);
+//            response.setStatus(HttpServletResponse.SC_OK);
+
+        } catch (NullPointerException e) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(401);
+            response.getWriter().println("Unauthorized");
+//            response.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 }
